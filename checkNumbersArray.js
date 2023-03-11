e34627dc3a1a7dfba9ef632a5e4e103f3e50e909
dorjepa <dorjepa@gmail.com>
2023-03-11 14:45:17 +0700

    Update checkNumbersArray.js

--- checkNumbersArray.js
+++ checkNumbersArray.js
@@ -1,4 +1,3 @@
-
 // ******
 // A function that returns true if the resulting array 
 // consists of an ascending sequence of numbers. 
@@ -6,121 +5,121 @@
 // Otherwise, it returns false.
 // ******
 
-    // Swaps two array elements
-    function SwapElemets(array, a, b) {
-        [array[a], array[b]] = [array[b], array[a]]
-        return +array.join('')
+// Swaps two array elements
+function SwapElemets(array, a, b) {
+    [array[a], array[b]] = [array[b], array[a]]
+    return +array.join('')
+}
+
+// Compare adjacent array elements
+function CheckElemets(current, prevElement, ascending = true) {
+
+    // Convert number to array
+    let num = current;
+    let currentArray = [];
+    while(num > 0){
+        currentArray.unshift(num % 10);
+        num = num / 10 | 0;
     }
 
-    // Compare adjacent array elements
-    function CheckElemets(current, prevElement, ascending = true) {
-
-        // Convert number to array
-        let num = current;
-        let currentArray = [];
-        while(num > 0){
-            currentArray.unshift(num % 10);
-            num = num / 10 | 0;
-        }
-
-        // We go through all possible combinations of permutations of digits in a number
-        const arrayNumbers = [];
-        currentArray.forEach(function(item, i, currentArray) {
-            currentArray.forEach(function(_item, _i, currentArray) {
-                const num = SwapElemets(currentArray, i, _i)
-                const direction = ascending ? 1 : -1
-                // We cut off numbers that do not satisfy the conditions
-                if (num * direction > prevElement * direction) { 
-                    arrayNumbers.push(num)
-                }
-            });
+    // We go through all possible combinations of permutations of digits in a number
+    const arrayNumbers = [];
+    currentArray.forEach(function(item, i, currentArray) {
+        currentArray.forEach(function(_item, _i, currentArray) {
+            const num = SwapElemets(currentArray, i, _i)
+            const direction = ascending ? 1 : -1
+            // We cut off numbers that do not satisfy the conditions
+            if (num * direction > prevElement * direction) { 
+                arrayNumbers.push(num)
+            }
         });
-        // Choose the nearest number
-        let newElement;
-        if (ascending) {
-            newElement = Math.min(...arrayNumbers);
+    });
+    // Choose the nearest number
+    let newElement;
+    if (ascending) {
+        newElement = Math.min(...arrayNumbers);
+    }
+    else {
+        newElement = Math.max(...arrayNumbers);
+    }
+    // console.log('arrayNumbers: ', arrayNumbers)
+    // console.log('newElement: ', newElement)
+    return newElement
+}
+
+// Checking the array
+function CheckArray(array) {
+    // console.log('array', array)
+    let result
+    let numberChanges = 0
+    const limitChanges = 1 // count of possible changes
+    for (let i = 0; i < array.length; i++)
+    {
+        if (i == 0) {
+            // console.log(i, ':', array[i])
+            result = array
+        }
+        else if (array[i-1] < array[i]) {
+            // console.log(i, ':', array[i])
+            result = array
+        }
+        // If the number does not meet the conditions, and it can be changed
+        else if(array[i] > 9 && numberChanges < limitChanges) {
+            numberChanges++
+            // console.log(i, ':', '--', array[i])
+
+            let newElement = CheckElemets(array[i], array[i-1], true)
+
+            // console.log(i, ':', newElement)
+            const current = array[i]
+            array[i] = newElement
+            // If increasing the current number did not give a positive result, 
+            // we try to decrease the previous one
+            if (newElement == 'Infinity') {
+                // console.log('current', current)
+                // console.log('prevElement', array[i-1])
+                newElement = CheckElemets(array[i-1], current, false)
+                // console.log(i - 1, ':', newElement)
+                array[i - 1] = newElement
+                array[i] = current
+                if (i > 1 && array[i - 1] <= array[i - 2]) {
+                    result = false
+                    break
+                }
+            }
+
+            result = array
         }
         else {
-            newElement = Math.max(...arrayNumbers);
+            // console.log(i, ':', '--0')
+            result = false
+            break
         }
-        // console.log('arrayNumbers: ', arrayNumbers)
-        // console.log('newElement: ', newElement)
-        return newElement
+
     }
+    return result
+}
 
-    // Checking the array
-    function CheckArray(array) {
-        console.log('array', array)
-        let result
-        let numberChanges = 0
-        const limitChanges = 1 // count of possible changes
-        for (let i = 0; i < array.length; i++)
-        {
-            if (i == 0) {
-                console.log(i, ':', array[i])
-                result = array
-            }
-            else if (array[i-1] < array[i]) {
-                console.log(i, ':', array[i])
-                result = array
-            }
-            // If the number does not meet the conditions, and it can be changed
-            else if(array[i] > 9 && numberChanges < limitChanges) {
-                numberChanges++
-                console.log(i, ':', '--', array[i])
+function Solution(numbers) {
 
-                let newElement = CheckElemets(array[i], array[i-1], true)
-
-                console.log(i, ':', newElement)
-                const current = array[i]
-                array[i] = newElement
-                // If increasing the current number did not give a positive result, 
-                // we try to decrease the previous one
-                if (newElement == 'Infinity') {
-                    console.log('current', current)
-                    console.log('prevElement', array[i-1])
-                    newElement = CheckElemets(array[i-1], current, false)
-                    console.log(i - 1, ':', newElement)
-                    array[i - 1] = newElement
-                    array[i] = current
-                    if (i > 1 && array[i - 1] <= array[i - 2]) {
-                        result = false
-                        break
-                    }
-                }
-
-                result = array
-            }
-            else {
-                console.log(i, ':', '--0')
-                result = false
-                break
-            }
-
+    if (numbers.length > 1) {
+        let result = CheckArray(numbers)
+        // console.log('newNumbers', numbers)
+        // console.log('result', result)
+        if (result == false) {
+            return false
+        }
+        else {
+            return true
         }
         return result
     }
-
-    function Solution(numbers) {
-
-        if (numbers.length > 1) {
-            let result = CheckArray(numbers)
-            // console.log('newNumbers', numbers)
-            // console.log('result', result)
-            if (result == false) {
-                return false
-            }
-            else {
-                return true
-            }
-            return result
-        }
-        else {
-            console.log('result', false)
-            return false
-        }
-        
+    else {
+        // console.log('result', false)
+        return false
     }
+
+}
 const a1 = [1, 5, 10, 20]
 const a2 = [1, 3, 900, 10]
 const a3 = [13, 31, 30]
